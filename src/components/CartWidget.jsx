0005251f7a087eeefb047fada2b434806b2ceb02
Renzo Castiglione <castiglione.renzo@gmail.com>
2025-05-29 21:04:00 -0300

    inicio de proyecto, navbar, widget, container

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,20 @@
+import { ShoppingCartIcon } from '@heroicons/react/24/outline'
+
+function CartWidget() {
+  return (
+    <button
+        type="button"
+        className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden"
+    >
+        <span className="absolute -inset-1.5" />
+        <span className="sr-only">View notifications</span>
+        <div className="relative">
+        <ShoppingCartIcon className="h-8 w-8 text-gray-700" />
+            <span className="absolute -top-2 -right-2 bg-red-600 text-white text-xs font-bold px-1.5 py-0.5 rounded-full">
+            3
+            </span>
+        </div>
+    </button>
+  );
+}
+export default CartWidget;
---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,9 @@
+function ItemListContainer({ greeting }) {
+  return (
+    <div className="item-list-container">
+      <h1>{greeting}</h1>
+      <p>Here you will find a list of items.</p>
+    </div>
+  );
+}
+export default ItemListContainer;
