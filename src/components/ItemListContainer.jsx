e05a87c981ba226bbfe549f371793ad53f135c1d
Renzo <castiglione.renzo@gmail.com>
2025-06-11 23:13:16 -0300

    fetch

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -34,23 +34,34 @@ const products = [
   // More products...
 ]
 
-
+import React, { useState, useEffect } from 'react';
 function ItemListContainer({ greeting }) {
+
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then((response) => response.json())
+      .then((data) => {
+        setItems(data);
+      })
+      .catch((error) => console.error('Error fetching products:', error));
+  }, []);
+
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 className="sr-only">Products</h2>
 
         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {products.map((product) => (
-            <a key={product.id} href={product.href} className="group">
+          {items.map((product) => (
+            <a key={product.id} className="group">
               <img
-                alt={product.imageAlt}
-                src={product.imageSrc}
+                alt={product.image}
+                src={product.image}
                 className="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-7/8"
               />
-              <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
-              <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
+              <h3 className="mt-4 text-sm text-gray-700">{product.title}</h3>
+              <p className="mt-1 text-lg font-medium text-gray-900">${product.price}</p>
             </a>
           ))}
         </div>
